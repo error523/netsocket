f7eeb95b48cc22c8956e272f332960e626efc4b9
mitewater <errorjiushierror@gmail.com>
2017-02-17 17:28:16 +0800

    更新mina简单样例

--- src/main/java/com/mitewater/net/mina/MinaSocketClient.java
+++ src/main/java/com/mitewater/net/mina/MinaSocketClient.java
@@ -1,5 +1,15 @@
 package com.mitewater.net.mina;
 
+import org.apache.mina.core.future.ConnectFuture;
+import org.apache.mina.core.service.IoConnector;
+import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IoSession;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.serialization.ObjectSerializationCodecFactory;
+import org.apache.mina.transport.socket.nio.NioSocketConnector;
+
+import java.net.InetSocketAddress;
+
 /**
  * Created with IntelliJ IDEA.
  * User: MiteWater
@@ -8,5 +18,22 @@ package com.mitewater.net.mina;
  * Description: class for netsocket try it self
  */
 public class MinaSocketClient {
+
+    public static void main(String[] args) {
+        //创建连接器
+        IoConnector connector = new NioSocketConnector(1);
+        //创建消息序列化器
+        connector.getFilterChain().addLast("stringserilize",
+                new ProtocolCodecFilter(new ObjectSerializationCodecFactory()));
+        connector.setHandler(new IoHandlerAdapter(){
+            @Override
+            public void messageReceived(IoSession session, Object message) throws Exception {
+                System.out.println("从服务器接收到的消息是:"+message);
+            }
+        });
+        ConnectFuture connectFuture = connector.connect(new InetSocketAddress("127.0.0.1",8000));
+        connectFuture.awaitUninterruptibly();
+        connectFuture.getSession().write("访问服务器");
+    }
 
 }
--- src/main/java/com/mitewater/net/mina/MinaSocketServer.java
+++ src/main/java/com/mitewater/net/mina/MinaSocketServer.java
@@ -1,5 +1,17 @@
 package com.mitewater.net.mina;
 
+import org.apache.mina.core.service.IoAcceptor;
+import org.apache.mina.core.service.IoHandler;
+import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IoSession;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.serialization.ObjectSerializationCodecFactory;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+
 /**
  * Created with IntelliJ IDEA.
  * User: MiteWater
@@ -8,4 +20,29 @@ package com.mitewater.net.mina;
  * Description: class for netsocket try it self
  */
 public class MinaSocketServer {
+    public static void main(String[] args) {
+        //创建一个单线程的服务器
+        IoAcceptor ioAcceptor = new NioSocketAcceptor(1);
+        //添加序列化，反序列话解析器
+        ioAcceptor.getFilterChain().addLast("stringserialize",
+                new ProtocolCodecFilter(new ObjectSerializationCodecFactory()));
+        IoHandler handler = new IoHandlerAdapter(){
+            @Override
+            public void messageReceived(IoSession session, Object message){
+                System.out.println("服务器收到了:"+message.toString());
+                session.write("返回一条信息给客户端");
+            }
+        };
+        SocketAddress address = new InetSocketAddress("127.0.0.1",8000);
+        try {
+            //绑定处理句柄，这个放在bind前面
+            ioAcceptor.setHandler(handler);
+            //绑定监听地址
+            ioAcceptor.bind(address);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
